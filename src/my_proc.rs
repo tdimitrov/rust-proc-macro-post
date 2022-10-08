fda3d64412180192d2bd0a8a8823b8daf8595d6d
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2022-10-08 23:14:57 +0300

    Stitching together result from functions with `TokenStream`

--- src/my_proc.rs
+++ src/my_proc.rs
@@ -2,5 +2,20 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 pub fn my_proc_impl(input: TokenStream) -> TokenStream {
-    quote!(println!("Answer: {}", #input))
+    let mut result = Vec::new();
+
+    result.push(progress_message("Thinking about the answer".to_string()));
+    result.push(answer(input));
+
+    quote!(
+        #(#result);*
+    )
+}
+
+fn progress_message(msg: String) -> TokenStream {
+    quote!(println!(#msg))
+}
+
+fn answer(result: TokenStream) -> TokenStream {
+    quote!(println!("Answer: {}", #result))
 }
