5b81c6f8e30ec3bd3e0372f953afec2ec38fe686
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2022-10-08 23:11:31 +0300

    Stitching together result from functions

--- src/my_proc.rs
+++ src/my_proc.rs
@@ -1,6 +1,21 @@
 use proc_macro2::TokenStream;
 use quote::quote;
+use syn::{parse_quote, ExprMacro};
 
 pub fn my_proc_impl(input: TokenStream) -> TokenStream {
-    quote!(println!("Answer: {}", #input))
+    let progress = progress_message("Thinking about the answer".to_string());
+    let answer = answer(input);
+
+    quote!(
+        #progress;
+        #answer;
+    )
+}
+
+fn progress_message(msg: String) -> ExprMacro {
+    parse_quote!(println!(#msg))
+}
+
+fn answer(result: TokenStream) -> ExprMacro {
+    parse_quote!(println!("Answer: {}", #result))
 }
